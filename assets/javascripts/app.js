92734a6b047d07d6fb8a1e1b600cde4f2e375d61
Carlos Vilhena <carlosvilhena@gmail.com>
2012-06-10 20:45:54 +0100

    fetch user data and show errors in not found

--- assets/javascripts/app.js
+++ assets/javascripts/app.js
@@ -12,6 +12,7 @@ App.Contribution = Ember.Object.extend({
 // Controllers
 App.contributionController = Ember.ArrayController.create({
   content: [],
+  error: null,
 
   createContribution: function(project_name, project_url) {
     var contribution = App.Contribution.create({ project_name: project_name, project_url: project_url });
@@ -20,6 +21,24 @@ App.contributionController = Ember.ArrayController.create({
 
   clearContributions: function() {
     this.set("content", []);
+  },
+
+  fetchUserContributions: function(username) {
+    $.ajax({
+      url: "https://api.github.com/users/" + username,
+      success: function(data) {
+        console.log("success");
+        console.log(data);
+      },
+      error: function(e) {
+        console.log("error");
+        if(e.status == 404)
+          App.contributionController.set("error","Oops... It appears that the user '" + username + "' doesn't exist...");
+        else
+          App.contributionController.set("error","Oops... something went wrong, plesse try again!");
+      },
+      dataType: "json"
+    });
   }
 
 });
@@ -29,18 +48,25 @@ App.contributionController = Ember.ArrayController.create({
 App.SearchUserView = Ember.TextField.extend({
   insertNewline: function() {
     var value = this.get('value');
-    console.log("Value:");
-    console.log(value);
     if (value) {
+      if (App.contributionController.fetchUserContributions(value)) {
+        console.log("The use is valid!!");
+      }
+      else {
+        console.log("Invalid user :(");
+      }
       App.contributionController.createContribution(value, value);
       this.set('value', '');
     }
   }
 });
 
+App.errorView = Ember.View.extend({
+  template: Ember.Handlebars.compile("{{error}}")
+});
 
 App.SearchingView = Ember.View.extend({
-  template: Ember.Handlebars.compile("<p>searching</p>")
+  template: Ember.Handlebars.compile("<p>no contributions yet</p>")
 });
 
 App.contributionView = Ember.View.extend({
