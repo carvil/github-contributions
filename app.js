f276449890a52d3ecbcc843a9cd6a73664bf04b3
Carlos Vilhena <carlosvilhena@gmail.com>
2012-06-13 23:16:53 +0100

    set the right port number

--- app.js
+++ app.js
@@ -37,6 +37,8 @@ app.get('/', function(req, res){
   });
 });
 
-app.listen(3000, function(){
+var port = process.env.PORT || 5000;
+
+app.listen(port, function(){
   console.log("Express server listening on port %d in %s mode", app.address().port, app.settings.env);
 });
